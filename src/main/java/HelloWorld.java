ad37d6ba6aee56ba230cb87792acab9c1737c1b9
ambet <ambet@192.168.2.26>
2013-09-15 19:46:59 +0800

    commit new

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello World! \n Great and Wonderful things");
+        resp.getWriter().print("Albert's First App");
     }
 
     public static void main(String[] args) throws Exception{
